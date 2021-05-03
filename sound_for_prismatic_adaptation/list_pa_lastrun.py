3e648b1eb200091812d3b55127b60fca563b077a
remi-lafitte <remi.jean.lafitte@protonmail.com>
2021-05-03 17:16:31 +0200

    commit

--- sound_for_prismatic_adaptation/list_pa_lastrun.py
+++ sound_for_prismatic_adaptation/list_pa_lastrun.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v2021.1.4),
-    on avril 23, 2021, at 13:22
+    on mai 03, 2021, at 15:23
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -51,7 +51,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='C:\\Users\\remi.DESKTOP-UI81QOM\\Desktop\\eprime_vv_lm\\sound_for_prismatic_adaptation\\list_pa_lastrun.py',
+    originPath='C:\\Users\\remi.DESKTOP-UI81QOM\\Desktop\\_EXPE_REMI_LAFITTE_300421\\eprime_vv_lm_290421\\sound_for_prismatic_adaptation\\list_pa_lastrun.py',
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
@@ -86,102 +86,228 @@ sound_exposure = sound.Sound('A', secs=0.8, stereo=True, hamming=True,
     name='sound_exposure')
 sound_exposure.setVolume(1)
 
+# Initialize components for Routine "break_2"
+break_2Clock = core.Clock()
+text = visual.TextStim(win=win, name='text',
+    text='',
+    font='Open Sans',
+    pos=(0, 0), height=0.1, wrapWidth=None, ori=0.0, 
+    color='white', colorSpace='rgb', opacity=None, 
+    languageStyle='LTR',
+    depth=0.0);
+
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
 routineTimer = core.CountdownTimer()  # to track time remaining of each (non-slip) routine 
 
 # set up handler to look after randomisation of conditions etc
-trial_loop = data.TrialHandler(nReps=1, method='random', 
+trials = data.TrialHandler(nReps=5.0, method='random', 
     extraInfo=expInfo, originPath=-1,
-    trialList=data.importConditions('stimuli.xlsx'),
-    seed=None, name='trial_loop')
-thisExp.addLoop(trial_loop)  # add the loop to the experiment
-thisTrial_loop = trial_loop.trialList[0]  # so we can initialise stimuli with some values
-# abbreviate parameter names if possible (e.g. rgb = thisTrial_loop.rgb)
-if thisTrial_loop != None:
-    for paramName in thisTrial_loop:
-        exec('{} = thisTrial_loop[paramName]'.format(paramName))
+    trialList=[None],
+    seed=None, name='trials')
+thisExp.addLoop(trials)  # add the loop to the experiment
+thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
+# abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+if thisTrial != None:
+    for paramName in thisTrial:
+        exec('{} = thisTrial[paramName]'.format(paramName))
 
-for thisTrial_loop in trial_loop:
-    currentLoop = trial_loop
+for thisTrial in trials:
+    currentLoop = trials
+    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
+    if thisTrial != None:
+        for paramName in thisTrial:
+            exec('{} = thisTrial[paramName]'.format(paramName))
+    
+    # set up handler to look after randomisation of conditions etc
+    trial_loop = data.TrialHandler(nReps=1, method='random', 
+        extraInfo=expInfo, originPath=-1,
+        trialList=data.importConditions('stimuli.xlsx'),
+        seed=None, name='trial_loop')
+    thisExp.addLoop(trial_loop)  # add the loop to the experiment
+    thisTrial_loop = trial_loop.trialList[0]  # so we can initialise stimuli with some values
     # abbreviate parameter names if possible (e.g. rgb = thisTrial_loop.rgb)
     if thisTrial_loop != None:
         for paramName in thisTrial_loop:
             exec('{} = thisTrial_loop[paramName]'.format(paramName))
     
-    # ------Prepare to start Routine "trial"-------
-    continueRoutine = True
-    # update component parameters for each repeat
-    sound_exposure.setSound(word_stimuli, secs=0.8, hamming=True)
-    sound_exposure.setVolume(1, log=False)
-    # keep track of which components have finished
-    trialComponents = [sound_exposure]
-    for thisComponent in trialComponents:
-        thisComponent.tStart = None
-        thisComponent.tStop = None
-        thisComponent.tStartRefresh = None
-        thisComponent.tStopRefresh = None
-        if hasattr(thisComponent, 'status'):
-            thisComponent.status = NOT_STARTED
-    # reset timers
-    t = 0
-    _timeToFirstFrame = win.getFutureFlipTime(clock="now")
-    trialClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
-    frameN = -1
-    
-    # -------Run Routine "trial"-------
-    while continueRoutine:
-        # get current time
-        t = trialClock.getTime()
-        tThisFlip = win.getFutureFlipTime(clock=trialClock)
-        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
-        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
-        # update/draw components on each frame
-        # start/stop sound_exposure
-        if sound_exposure.status == NOT_STARTED and tThisFlip >= isi-frameTolerance:
-            # keep track of start time/frame for later
-            sound_exposure.frameNStart = frameN  # exact frame index
-            sound_exposure.tStart = t  # local t and not account for scr refresh
-            sound_exposure.tStartRefresh = tThisFlipGlobal  # on global time
-            sound_exposure.play(when=win)  # sync with win flip
-        if sound_exposure.status == STARTED:
-            # is it time to stop? (based on global clock, using actual start)
-            if tThisFlipGlobal > sound_exposure.tStartRefresh + 0.8-frameTolerance:
-                # keep track of stop time/frame for later
-                sound_exposure.tStop = t  # not accounting for scr refresh
-                sound_exposure.frameNStop = frameN  # exact frame index
-                win.timeOnFlip(sound_exposure, 'tStopRefresh')  # time at next scr refresh
-                sound_exposure.stop()
+    for thisTrial_loop in trial_loop:
+        currentLoop = trial_loop
+        # abbreviate parameter names if possible (e.g. rgb = thisTrial_loop.rgb)
+        if thisTrial_loop != None:
+            for paramName in thisTrial_loop:
+                exec('{} = thisTrial_loop[paramName]'.format(paramName))
         
-        # check for quit (typically the Esc key)
-        if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
-            core.quit()
-        
-        # check if all components have finished
-        if not continueRoutine:  # a component has requested a forced-end of Routine
-            break
-        continueRoutine = False  # will revert to True if at least one component still running
+        # ------Prepare to start Routine "trial"-------
+        continueRoutine = True
+        # update component parameters for each repeat
+        sound_exposure.setSound(word_stimuli, secs=0.8, hamming=True)
+        sound_exposure.setVolume(1, log=False)
+        # keep track of which components have finished
+        trialComponents = [sound_exposure]
         for thisComponent in trialComponents:
-            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                continueRoutine = True
-                break  # at least one component has not yet finished
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        trialClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+        frameN = -1
         
-        # refresh the screen
-        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
-            win.flip()
+        # -------Run Routine "trial"-------
+        while continueRoutine:
+            # get current time
+            t = trialClock.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=trialClock)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            # start/stop sound_exposure
+            if sound_exposure.status == NOT_STARTED and tThisFlip >= isi-frameTolerance:
+                # keep track of start time/frame for later
+                sound_exposure.frameNStart = frameN  # exact frame index
+                sound_exposure.tStart = t  # local t and not account for scr refresh
+                sound_exposure.tStartRefresh = tThisFlipGlobal  # on global time
+                sound_exposure.play(when=win)  # sync with win flip
+            if sound_exposure.status == STARTED:
+                # is it time to stop? (based on global clock, using actual start)
+                if tThisFlipGlobal > sound_exposure.tStartRefresh + 0.8-frameTolerance:
+                    # keep track of stop time/frame for later
+                    sound_exposure.tStop = t  # not accounting for scr refresh
+                    sound_exposure.frameNStop = frameN  # exact frame index
+                    win.timeOnFlip(sound_exposure, 'tStopRefresh')  # time at next scr refresh
+                    sound_exposure.stop()
+            
+            # check for quit (typically the Esc key)
+            if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+                core.quit()
+            
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in trialComponents:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+            
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+        
+        # -------Ending Routine "trial"-------
+        for thisComponent in trialComponents:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        sound_exposure.stop()  # ensure sound has stopped at end of routine
+        trial_loop.addData('sound_exposure.started', sound_exposure.tStartRefresh)
+        trial_loop.addData('sound_exposure.stopped', sound_exposure.tStopRefresh)
+        # the Routine "trial" was not non-slip safe, so reset the non-slip timer
+        routineTimer.reset()
+        thisExp.nextEntry()
+        
+    # completed 1 repeats of 'trial_loop'
     
-    # -------Ending Routine "trial"-------
-    for thisComponent in trialComponents:
-        if hasattr(thisComponent, "setAutoDraw"):
-            thisComponent.setAutoDraw(False)
-    sound_exposure.stop()  # ensure sound has stopped at end of routine
-    trial_loop.addData('sound_exposure.started', sound_exposure.tStartRefresh)
-    trial_loop.addData('sound_exposure.stopped', sound_exposure.tStopRefresh)
-    # the Routine "trial" was not non-slip safe, so reset the non-slip timer
-    routineTimer.reset()
+    
+    # set up handler to look after randomisation of conditions etc
+    trials_2 = data.TrialHandler(nReps=1.0, method='sequential', 
+        extraInfo=expInfo, originPath=-1,
+        trialList=data.importConditions('countdown.xlsx'),
+        seed=None, name='trials_2')
+    thisExp.addLoop(trials_2)  # add the loop to the experiment
+    thisTrial_2 = trials_2.trialList[0]  # so we can initialise stimuli with some values
+    # abbreviate parameter names if possible (e.g. rgb = thisTrial_2.rgb)
+    if thisTrial_2 != None:
+        for paramName in thisTrial_2:
+            exec('{} = thisTrial_2[paramName]'.format(paramName))
+    
+    for thisTrial_2 in trials_2:
+        currentLoop = trials_2
+        # abbreviate parameter names if possible (e.g. rgb = thisTrial_2.rgb)
+        if thisTrial_2 != None:
+            for paramName in thisTrial_2:
+                exec('{} = thisTrial_2[paramName]'.format(paramName))
+        
+        # ------Prepare to start Routine "break_2"-------
+        continueRoutine = True
+        routineTimer.add(1.000000)
+        # update component parameters for each repeat
+        text.setText(countdown)
+        # keep track of which components have finished
+        break_2Components = [text]
+        for thisComponent in break_2Components:
+            thisComponent.tStart = None
+            thisComponent.tStop = None
+            thisComponent.tStartRefresh = None
+            thisComponent.tStopRefresh = None
+            if hasattr(thisComponent, 'status'):
+                thisComponent.status = NOT_STARTED
+        # reset timers
+        t = 0
+        _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+        break_2Clock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+        frameN = -1
+        
+        # -------Run Routine "break_2"-------
+        while continueRoutine and routineTimer.getTime() > 0:
+            # get current time
+            t = break_2Clock.getTime()
+            tThisFlip = win.getFutureFlipTime(clock=break_2Clock)
+            tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+            frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+            # update/draw components on each frame
+            
+            # *text* updates
+            if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+                # keep track of start time/frame for later
+                text.frameNStart = frameN  # exact frame index
+                text.tStart = t  # local t and not account for scr refresh
+                text.tStartRefresh = tThisFlipGlobal  # on global time
+                win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+                text.setAutoDraw(True)
+            if text.status == STARTED:
+                # is it time to stop? (based on global clock, using actual start)
+                if tThisFlipGlobal > text.tStartRefresh + 1.0-frameTolerance:
+                    # keep track of stop time/frame for later
+                    text.tStop = t  # not accounting for scr refresh
+                    text.frameNStop = frameN  # exact frame index
+                    win.timeOnFlip(text, 'tStopRefresh')  # time at next scr refresh
+                    text.setAutoDraw(False)
+            
+            # check for quit (typically the Esc key)
+            if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+                core.quit()
+            
+            # check if all components have finished
+            if not continueRoutine:  # a component has requested a forced-end of Routine
+                break
+            continueRoutine = False  # will revert to True if at least one component still running
+            for thisComponent in break_2Components:
+                if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                    continueRoutine = True
+                    break  # at least one component has not yet finished
+            
+            # refresh the screen
+            if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+                win.flip()
+        
+        # -------Ending Routine "break_2"-------
+        for thisComponent in break_2Components:
+            if hasattr(thisComponent, "setAutoDraw"):
+                thisComponent.setAutoDraw(False)
+        trials_2.addData('text.started', text.tStartRefresh)
+        trials_2.addData('text.stopped', text.tStopRefresh)
+        thisExp.nextEntry()
+        
+    # completed 1.0 repeats of 'trials_2'
+    
     thisExp.nextEntry()
     
-# completed 1 repeats of 'trial_loop'
+# completed 5.0 repeats of 'trials'
 
 
 # Flip one final time so any remaining win.callOnFlip() 
